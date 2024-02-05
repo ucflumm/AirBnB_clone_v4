c6da4b13172c250ef6102b1e6856e738ec628d75
ucflumm <ucflumm@gmail.com>
2024-02-05 23:23:06 +1100

    Refactor AJAX request and add console log

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -23,32 +23,40 @@ $(document).ready(function () {
     }
   });
 
-  $.post('/api/v1/places_search/', '{}', function (data, textStatus) {
-    if (textStatus === 'success') {
-      for (const place of data) {
-        const article = $('<article></article>').addClass('place');
-        const titleBox = $('<div></div>').addClass('title_box');
-        const title = $('<h2></h2>').text(place.name);
-        const price = $('<div></div>').addClass('price_by_night').text('$' + place.price_by_night);
-        titleBox.append(title, price);
-        const information = $('<div></div>').addClass('information');
-        const maxGuest = $('<div></div>').addClass('max_guest').text(place.max_guest + ' Guests');
-        const numberRooms = $('<div></div>').addClass('number_rooms').text(place.number_rooms + ' Rooms');
-        const numberBathrooms = $('<div></div>').addClass('number_bathrooms').text(place.number_bathrooms + ' Bathrooms');
-        information.append(maxGuest, numberRooms, numberBathrooms);
-        const description = $('<div></div>').addClass('description').text(place.description);
-        article.append(titleBox, information, description);
-        $('section.places').append(article);
-        console.log(article)
+  function fetchPlaces(data = {}) {
+    $.ajax({
+      type: 'POST',
+      url: '/api/v1/places_search/',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      success: function (data, textStatus) {
+        if (textStatus === 'success') {
+          $('section.places').empty();
+          for (const place of data) {
+            const article = $('<article></article>').addClass('place');
+            const titleBox = $('<div></div>').addClass('title_box');
+            const title = $('<h2></h2>').text(place.name);
+            const price = $('<div></div>').addClass('price_by_night').text('$' + place.price_by_night);
+            titleBox.append(title, price);
+            const information = $('<div></div>').addClass('information');
+            const maxGuest = $('<div></div>').addClass('max_guest').text(place.max_guest + ' Guests');
+            const numberRooms = $('<div></div>').addClass('number_rooms').text(place.number_rooms + ' Rooms');
+            const numberBathrooms = $('<div></div>').addClass('number_bathrooms').text(place.number_bathrooms + ' Bathrooms');
+            information.append(maxGuest, numberRooms, numberBathrooms);
+            const description = $('<div></div>').addClass('description').text(place.description);
+            article.append(titleBox, information, description);
+            $('section.places').append(article);
+          }
+        }
       }
-    }
-
-  });
+    });
+  }
 
   fetchPlaces();
 
   $('button').click(function () {
     const amenIds = Object.keys(checkedDict);
     fetchPlaces({'amenities': amenIds});
+    console.log('Button clicked');
   });
 });
