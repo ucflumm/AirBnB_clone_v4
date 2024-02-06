a72590ca6308b203562e21b509686b2bb16fa11f
jess6718 <6718@holbertonstudents.com>
2024-02-06 12:56:23 +0100

    fix filter bug

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -23,31 +23,43 @@ $(document).ready(function () {
     }
   });
 
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+
   function fetchPlaces(data = {}) {
     $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
       type: 'POST',
-      url: '/api/v1/places_search/',
       contentType: 'application/json',
-      data: JSON.stringify(data),
-      success: function (data, textStatus) {
-        if (textStatus === 'success') {
-          $('section.places').empty();
-          for (const place of data) {
-            const article = $('<article></article>').addClass('place');
-            const titleBox = $('<div></div>').addClass('title_box');
-            const title = $('<h2></h2>').text(place.name);
-            const price = $('<div></div>').addClass('price_by_night').text('$' + place.price_by_night);
-            titleBox.append(title, price);
-            const information = $('<div></div>').addClass('information');
-            const maxGuest = $('<div></div>').addClass('max_guest').text(place.max_guest + ' Guests');
-            const numberRooms = $('<div></div>').addClass('number_rooms').text(place.number_rooms + ' Rooms');
-            const numberBathrooms = $('<div></div>').addClass('number_bathrooms').text(place.number_bathrooms + ' Bathrooms');
-            information.append(maxGuest, numberRooms, numberBathrooms);
-            const description = $('<div></div>').addClass('description').text(place.description);
-            article.append(titleBox, information, description);
-            $('section.places').append(article);
-          }
+      data: JSON.stringify(data), // Empty object
+      success: function (response) {
+        $('section.places').empty();
+        for (const place of response) {
+          const article = $('<article></article>').addClass('place');
+          const titleBox = $('<div></div>').addClass('title_box');
+          const title = $('<h2></h2>').text(place.name);
+          const price = $('<div></div>').addClass('price_by_night').text('$' + place.price_by_night);
+          titleBox.append(title, price);
+          const information = $('<div></div>').addClass('information');
+          const maxGuest = $('<div></div>').addClass('max_guest').text(place.max_guest + ' Guests');
+          const numberRooms = $('<div></div>').addClass('number_rooms').text(place.number_rooms + ' Rooms');
+          const numberBathrooms = $('<div></div>').addClass('number_bathrooms').text(place.number_bathrooms + ' Bathrooms');
+          information.append(maxGuest, numberRooms, numberBathrooms);
+          const description = $('<div></div>').addClass('description').text(place.description);
+          article.append(titleBox, information, description);
+          $('section.places').append(article);
         }
+        console.log('Success:', response);
+      },
+      error: function (error) {
+        console.error('Error:', error);
       }
     });
   }
